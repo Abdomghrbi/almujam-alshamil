0ee7f4641ddecd67d201f6451e636ec63a135ea3
Abdullrahman almaghrabi <73081830+Abdomghrbi@users.noreply.github.com>
2026-06-09 03:08:17 +0300

    Create upload.js

---
+++ server/middleware/upload.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024 // 5MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype.startsWith('image/')
+    ) {
+      cb(null, true);
+    } else {
+      cb(
+        new Error('يسمح فقط برفع الصور')
+      );
+    }
+  }
+});
+
+module.exports = upload;
